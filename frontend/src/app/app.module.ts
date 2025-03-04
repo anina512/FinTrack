e18a3f0073e678329cb6fd6b6f31bf4df8289a85
Anina <aninanair99512@gmail.com>
2025-03-03 20:15:25 -0500

    Connected http client

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';  // ✅ Import this
+import { CommonModule } from '@angular/common';  
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';  
 import { RouterModule } from '@angular/router';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,6 +18,8 @@ import { HeaderComponent } from './shared/header/header.component';
 import { ExpenseComponent } from './pages/transactions/expense/expense.component';
 import { IncomeComponent } from './pages/transactions/income/income.component';
 import { BudgetComponent } from './pages/transactions/budget/budget.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
@@ -40,9 +43,11 @@ import { BudgetComponent } from './pages/transactions/budget/budget.component';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    RouterModule.forRoot([])
+    MatIconModule,  
+    RouterModule.forRoot([]),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
